ee9479abdc956b2530dda26f2c1687182ae9ba28
Rodrigo Santibanez <glucksfall@users.noreply.github.com>
2025-04-29 18:27:11 -0700

    Updated setup.py with missing python versions

--- setup.py
+++ setup.py
@@ -65,6 +65,9 @@ def main():
 			'Programming Language :: Python :: 3.8',
 			'Programming Language :: Python :: 3.9',
 			'Programming Language :: Python :: 3.10',
+			'Programming Language :: Python :: 3.11',
+			'Programming Language :: Python :: 3.12',
+			'Programming Language :: Python :: 3.13',
 
 			# other classifiers added by author
 			'Environment :: Console',
